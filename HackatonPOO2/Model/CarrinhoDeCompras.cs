aff35adb0d1858ff42da8efa9c42efe582489999
Eduardo Fadel <1eduardofadel@gmail.com>
2023-05-02 00:01:08 -0300

    opa 4

--- HackatonPOO2/Model/CarrinhoDeCompras.cs
+++ HackatonPOO2/Model/CarrinhoDeCompras.cs
@@ -1,6 +1,40 @@
 namespace HackatonPOO2.Model;
+using HackatonPOO2.UI;
 
 public class CarrinhoDeCompras
 {
-    
+    private double? valorTotal;
+    private List<Produto>? produtos;
+
+    public CarrinhoDeCompras()
+    {
+    }
+
+    public CarrinhoDeCompras(double valorTotal, List<Produto> produtos)
+    {
+        this.valorTotal = valorTotal;
+        this.produtos = produtos;
+    }
+
+    public double? ValorTotal
+    {
+        get { return valorTotal; }
+        set { this.valorTotal = value; }
+    }
+
+    public List<Produto>? Produtos
+    {
+        get { return produtos; }
+        set { this.produtos = value; }
+    }
+
+    public double? calcularValorTotal(List<Produto> produtos)
+    {
+        double? valorTotal = 0;
+        for (int i = 0; i < produtos.Count; i++)
+        {
+            valorTotal += produtos[i].Preco;
+        }
+        return valorTotal;
+    }
 }
